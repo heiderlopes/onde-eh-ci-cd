3cec5e84f7970e134c89191909f3fafc42f5529e
Heider Pinholi Lopes <heidertreinamentos@gmail.com>
2022-08-05 14:25:51 -0300

    Inclusao do projeto

---
+++ app/src/main/java/br/com/heiderlopes/ondeeh/APIService.kt
@@ -0,0 +1,23 @@
+package br.com.heiderlopes.ondeeh
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object APIService {
+    private var INSTANCE: AddressService? = null
+    val instance: AddressService?
+        get() {
+            if (INSTANCE == null) {
+                val client = OkHttpClient.Builder()
+                    .build()
+                val retrofit = Retrofit.Builder()
+                    .baseUrl(BuildConfig.BASE_URL_VIA_CEP)
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .client(client)
+                    .build()
+                INSTANCE = retrofit.create(AddressService::class.java)
+            }
+            return INSTANCE
+        }
+}
